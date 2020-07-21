d880aecba0d24f4f9a81ba290f316ba64913a2f6
adilay26 <64276083+adilay26@users.noreply.github.com>
2020-07-21 16:31:01 +0200

    First draft of the Fleet Shop - Front End

---
+++ js/fleet-shop.js
@@ -0,0 +1,33 @@
+// Index Page
+
+document.querySelectorAll('a[href^="#pricing"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('a[href^="#footer"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Invoice Page
+
+document.querySelectorAll('a[href^="#invoice"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
